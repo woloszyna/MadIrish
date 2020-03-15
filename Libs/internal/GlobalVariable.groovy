98e013b135ac51f142ea8bcbd10c3d95431e1708
Albert Woloszyn <awoloszyn@gmail.com>
2020-03-15 21:03:19 +0000

    Added profile

--- Libs/internal/GlobalVariable.groovy
+++ Libs/internal/GlobalVariable.groovy
@@ -9,31 +9,6 @@ import com.kms.katalon.core.main.TestCaseMain
  */
 public class GlobalVariable {
      
-    /**
-     * <p>Profile DemoSiteEnv1 : URL to the environment
-Profile DemoSiteEnv2 : URL to the environment
-Profile DemoSiteEnv3 : URL to the environment
-Profile DemoSiteEnv4 : URL to the environment
-Profile DemoSiteEnv5 : URL to the environment
-Profile DemoSiteEnv6 : URL to the environment</p>
-     */
-    public static Object URL
-     
-    /**
-     * <p>Profile DemoSiteEnv1 : Login for the user
-Profile DemoSiteEnv2 : Login for the user
-Profile DemoSiteEnv3 : Login for the user
-Profile DemoSiteEnv4 : Login for the user
-Profile DemoSiteEnv5 : Login for the user
-Profile DemoSiteEnv6 : Login for the user</p>
-     */
-    public static Object userName
-     
-    /**
-     * <p>Profile DemoSiteEnv1 : password for the user</p>
-     */
-    public static Object password
-     
 
     static {
         try {
@@ -41,9 +16,6 @@ Profile DemoSiteEnv6 : Login for the user</p>
 			selectedVariables += TestCaseMain.getGlobalVariables(RunConfiguration.getExecutionProfile())
             selectedVariables += RunConfiguration.getOverridingParameters()
     
-            URL = selectedVariables['URL']
-            userName = selectedVariables['userName']
-            password = selectedVariables['password']
             
         } catch (Exception e) {
             TestCaseMain.logGlobalVariableError(e)
